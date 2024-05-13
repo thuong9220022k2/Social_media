0be026862007d5354de848b3a574957031b79556
thuong <thuong9220022k2@gmail.com>
2024-05-13 23:41:28 +0700

    update

--- client/src/scenes/chatPage/index.jsx
+++ client/src/scenes/chatPage/index.jsx
@@ -3,10 +3,13 @@ import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from '../../components/ChatFeed';
 import "../../App.css";
 
+const projectID = '84475c91-fca9-4a5a-9f64-5555f1dbe498';
+
 const chatPage = () => {
     return (
         <ChatEngine
             height="100vh"
+            projectID={projectID}
             userName={localStorage.getItem('username')}
             userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
