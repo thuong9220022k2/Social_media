674b57b0f45a84ca82cec6580c88ddb1ea1dcc2f
Skuru23 <98009221+Skuru23@users.noreply.github.com>
2024-06-16 12:19:39 +0700

    fix account

--- client/src/constants/account.js
+++ client/src/constants/account.js
@@ -1,4 +1,4 @@
 export const accounts = [
-    {"id":1,  "email": "thuongda@gmail.com", "password": "12345678"},
-    {"id": 2, "email": "lmao@gmail.com", "password": "12345678"}
+    {"id":1,  "email": "gooduser@gmail.com", "password": "12345678"},
+    {"id": 2, "email": "hanabi@gmail.com", "password": "12345678"}
 ]
